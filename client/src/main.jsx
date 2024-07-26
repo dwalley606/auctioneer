5ace1192cababee05efbf538b40d96cfe2f7adb4
Daniel <dwalley606@gmail.com>
2024-07-26 13:23:03 -0400

    Seed working, added main and app.jsx, client loads

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,50 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import Home from "./pages/Home";
+// import Detail from "./pages/Detail";
+// import NoMatch from "./pages/NoMatch";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+// import Success from "./pages/Success";
+// import OrderHistory from "./pages/OrderHistory";
+import App from "./App.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    error: <NoMatch />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+      {
+        path: "/products/:id",
+        element: <Detail />,
+      },
+      {
+        path: "/orderHistory",
+        element: <OrderHistory />,
+      },
+      {
+        path: "/success",
+        element: <Success />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
